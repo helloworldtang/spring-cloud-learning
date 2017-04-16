06f07eb76a078df5ac69cffdac172dffcc640996
Tangcheng <helloworld.tang@qq.com>
2017-04-16 23:27:16 +0800

    fix bug. /info refactor

--- eureka-ribbon/src/main/java/com/learning/eureka/ribbon/service/ComputeService.java
+++ eureka-ribbon/src/main/java/com/learning/eureka/ribbon/service/ComputeService.java
@@ -1,19 +1,39 @@
 package com.learning.eureka.ribbon.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
 
 /**
  * Created by tang.cheng on 2017/3/23.
  */
 @Service
 public class ComputeService {
+    public static final Logger LOGGER = LoggerFactory.getLogger(ComputeService.class);
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @PostConstruct
+    public void testApi() {
+        try {
+            LOGGER.info("{}", serviceUrl());
+        } catch (Exception e) {
+            LOGGER.warn(e.getMessage(), e);
+        }
+    }
 
     public String serviceUrl() {
         String serviceName = "COMPUTE-SERVICE";
