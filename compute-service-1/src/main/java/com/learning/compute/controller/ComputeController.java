f2df39ee7ce700e31cee998333335951c457d975
唐成 <cheng.tang@gongbangbang.com>
2019-02-24 18:26:39 +0800

    optimize:use new Api

--- compute-service-1/src/main/java/com/learning/compute/controller/ComputeController.java
+++ compute-service-1/src/main/java/com/learning/compute/controller/ComputeController.java
@@ -1,10 +1,8 @@
 package com.learning.compute.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,25 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by tang.cheng on 2017/3/20.
  */
+@Slf4j
 @RestController
 public class ComputeController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ComputeController.class);
     @Autowired
-    private DiscoveryClient client;
+    private Registration registration;
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
-        ServiceInstance instance = client.getLocalServiceInstance();
         Integer r = a + b;
-        LOGGER.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        log.info("/add, host:" + registration.getHost() + ", service_id:" + registration.getServiceId() + ", result:" + r);
         return r;
     }
 
     @RequestMapping(value = "/minus", method = RequestMethod.GET)
     public Integer minus(@RequestParam Integer a, @RequestParam Integer b) {
-        ServiceInstance instance = client.getLocalServiceInstance();
         Integer r = a - b;
-        LOGGER.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        log.info("/add, host:" + registration.getHost() + ", service_id:" + registration.getServiceId() + ", result:" + r);
         return r;
     }
 
